aadbe689860ae94221916b6bc998f55f7d83aacd
Michail Chatzimanolakis <michaich@eu-a6-008-14.euler.ethz.ch>
2020-06-29 09:56:40 +0200

    Deleted some files

--- nested-sampling/tensor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#SBATCH --constraint=gpu
-#SBATCH --ntasks=100
-#SBATCH --time=00:40:00
-#SBATCH --job-name="tensor"
-#SBATCH --output=tensor.%j.o
-#SBATCH --error=tensor.%j.e
-srun -u -n 100 ./tensor.py --samples 100 --days 30
