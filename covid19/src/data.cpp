fe03bc0321fabcb38d42e41545871c41111ff871
Michail Chatzimanolakis <michaich@eu-login-15.euler.ethz.ch>
2020-10-26 12:28:11 +0100

    minor

--- covid19/src/data.cpp
+++ covid19/src/data.cpp
@@ -1,5 +1,4 @@
 #include "data.h"
-#include <utils/assert.h>
 
 namespace epidemics {
 
@@ -26,45 +25,5 @@ void ModelData::init() {
         }
     }
 }
-
-
-ModelData readModelData(const char *filename) {
-    FILE *f = fopen(filename, "r");
-    if (f == nullptr)
-        DIE("Error opening file \"%s\". Did you forget to run ./py/data.py?\n", filename);
-
-    int N;  // Number of regions.
-    if (fscanf(f, "%d", &N) != 1)
-        DIE("Reading number of regions failed.");
-
-    ModelData out;
-    out.regionKeys.resize(N);
-    for (int i = 0; i < N; ++i) {
-        char name[64];
-        if (fscanf(f, "%s", name) != 1)
-            DIE("Reading name of the region #%d failed.\n", i);
-        out.regionKeys[i] = name;
-    }
-
-    out.Ni.resize(N);
-    for (double &pop : out.Ni)
-        if (fscanf(f, "%lg", &pop) != 1)
-            DIE("Reading region population failed.\n");
-
-    out.Mij.resize(N * N);
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j) {
-            if (fscanf(f, "%lf", &out.Mij[i * N + j]) != 1)
-                DIE("Reading Mij[%d][%d] failed.\n", i, j);
-            if (i == j)
-                out.Mij[i * N + j] = 0.0;
-        }
-
-
-    fclose(f);
-
-    return out;
-}
-
 
 }  // namespace epidemics
