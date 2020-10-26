dc3aea3b4bab2bddbfe899b4a23d6d750040e057
Ivica Kicic <kicici@ethz.ch>
2020-10-26 12:17:27 +0100

    fix merge

--- covid19/src/data.cpp
+++ covid19/src/data.cpp
@@ -39,48 +39,4 @@ void ModelData::init() {
     }
 }
 
-
-ModelData readModelData(const char *filename) {
-    FILE *f = fopen(filename, "r");
-    if (f == nullptr)
-        DIE("Error opening file \"%s\". Did you forget to run ./py/data.py?\n", filename);
-
-    int N;  // Number of regions.
-    if (fscanf(f, "%d", &N) != 1)
-        DIE("Reading number of regions failed.");
-
-    ModelData out;
-    out.regionKeys.resize(N);
-    for (int i = 0; i < N; ++i) {
-        char name[64];
-        if (fscanf(f, "%s", name) != 1)
-            DIE("Reading name of the region #%d failed.\n", i);
-        out.regionKeys[i] = name;
-    }
-
-    out.Ni.resize(N);
-    for (double &pop : out.Ni)
-        if (fscanf(f, "%lg", &pop) != 1)
-            DIE("Reading region population failed.\n");
-
-    out.Mij.resize(N * N);
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j) {
-            if (fscanf(f, "%lf", &out.Mij[i * N + j]) != 1)
-                DIE("Reading Mij[%d][%d] failed.\n", i, j);
-            if (i == j)
-                out.Mij[i * N + j] = 0.0;
-        }
-
-    out.Ui.resize(N);
-    for (double &u : out.Ui)
-        if (fscanf(f, "%lg", &u) != 1)
-            DIE("Reading user-defined failed.\n");
-
-    fclose(f);
-
-    return out;
-}
-
-
 }  // namespace epidemics
