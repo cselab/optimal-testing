4ebe9803a83439b2d80fffae04284caf16321bf7
Michail Chatzimanolakis <michaich@eu-login-47.euler.ethz.ch>
2020-10-26 17:32:29 +0100

    launch script

---
+++ launch.sh
@@ -0,0 +1,11 @@
+CORES=4
+NLIVE=50
+CASE=2
+DLOGZ=0.1
+SAMPLES=10
+SENSORS=2
+NY=10
+
+python3 nested.py --nlive $NLIVE --case $CASE --dlogz $DLOGZ --cores $CORES
+mpirun -n $CORES ./samples.py --case $CASE --samples $SAMPLES
+mpirun -n $CORES ./run-sequential.py --nSensors $SENSORS --path "case$CASE" --Ny $NY --Ntheta $SAMPLES --case $CASE
